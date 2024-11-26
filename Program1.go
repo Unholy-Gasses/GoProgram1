f2f208b95a244215697ae08b5ad474e58fe7e3f3
Cole <cole.parsons8@gmail.com>
2024-11-26 17:34:00 -0500

    Program now visits site and gives back error if there is one and url

--- Program1.go
+++ Program1.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gocolly/colly"
 )
 
-type comic struct {
-	ImgUrl string `json:"imgurl"`
-}
-
 func main() {
-	c := colly.NewCollector()                               //creates a new instance of a Collector
-	c.OnHTML("div=id\"comic\"", func(h *colly.HTMLElement)) //Any time CSS Selector ("") finds what its looking for, run func(), (h *colly.HTMLElement)
+	// Corrected URL string
+	scrapeUrl := "https://xkcd.com/"
+
+	// Create a new Colly collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("xkcd.com"), // Allowed domain
+	)
+
+	// Define what to do when a matching HTML element is found
+	c.OnHTML("div#comic", func(h *colly.HTMLElement) {
+		fmt.Printf("Comic Text:", h.Text)
+	})
+	//A function that runs everytime a request goes through
+	c.OnRequest((func(r *colly.Request) {
+		fmt.Printf(fmt.Sprintf("Visiting %s", r.URL))
+	}))
+	//Prints error if it occurs
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Printf("Error while scraping: %s\n", e.Error())
+	})
+	// Visit URL
+	c.Visit(scrapeUrl)
 }
